bf609742eb06fa8029a942e76edc6762605601a7
Salam Al Yahya <salam0smy@gmail.com>
2026-03-01 21:21:41 +0300

    style: use semantic bg token in default layout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,7 @@
 <!-- app/layouts/default.vue -->
 <template>
   <UApp>
-    <div class="min-h-screen bg-gray-50">
+    <div class="min-h-screen bg-(--color-bg)">
       <slot />
     </div>
   </UApp>
